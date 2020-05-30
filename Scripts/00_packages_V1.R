2fb167b21c22198c57d7a2af4b9f056c0382d1ca
Winer Daniel Reyes Corral <winerdreyes@gmail.com>
2020-05-30 18:16:10 -0400

    Installed packages simr and lme4 for power analysis

---
+++ Scripts/00_packages_V1.R
@@ -0,0 +1,10 @@
+##############################################################
+################# Comp 598 Final Project #####################
+###################    Power Analysis  #######################
+##############################################################
+# Script to install packages and libraries
+install.packages("simr")
+install.packages("lme4")
+
+library(simr)
+library(lme4)
